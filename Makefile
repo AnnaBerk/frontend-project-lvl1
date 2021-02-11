1dc691dc0d192130bfcf4bd48b9d124bd04a5657
Бернова Анна <bast33@yandex.ru>
2021-02-11 18:49:08 +0300

    fix linter

--- Makefile
+++ Makefile
@@ -8,4 +8,3 @@ publish:
 lint:
 	npx eslint .
 
-
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 export const startGame = () => {
-console.log("Welcome to the Brain Games!");
-const username = readlineSync.question('May I have your name? ');
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name? ');
   console.log(`\nHello, ${username}!\n`);
 };
