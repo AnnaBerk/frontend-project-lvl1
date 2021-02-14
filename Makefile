1f5bac03cbc0d4df8ec17d953e2c66890f49c0fd
Бернова Анна <bast33@yandex.ru>
2021-02-14 20:25:14 +0300

    lesson 6

--- Makefile
+++ Makefile
@@ -1,10 +1,7 @@
 
-install:
-	npm install
-brain-games:
-	node bin/brain-games.js
-publish:
-	npm publish --dry-run
-lint:
-	npx eslint .
+install:	npm install
+brain-games:	node bin/brain-games.js
+publish:	npm publish --dry-run
+lint:	npx eslint .
+even:	node src/bin/brain-even.js	
 
---
+++ src/games/game-calc.js
@@ -0,0 +1,26 @@
+import { gameData, startGame } from '../cli.js';
+import generateNumber from '../lib/generateNumber.js';
+
+const description = 'What is the result of the expression?';
+
+const questionAnswer = () => {
+  const firstNumber = generateNumber(1, 10);
+  const secondNumber = generateNumber(1, 10);
+  const operation = generateNumber(1, 3);
+  if (operation === 3) {
+    const question = `${firstNumber} + ${secondNumber}`;
+    const answer = firstNumber + secondNumber;
+    return gameData(question, String(answer));
+  } else if (operation === 2) {
+    const question = `${firstNumber} - ${secondNumber}`;
+    const answer = firstNumber - secondNumber;
+    return gameData(question, String(answer));
+  }
+  const question = `${firstNumber} * ${secondNumber}`;
+  const answer = firstNumber * secondNumber;
+  return gameData(question, String(answer));
+};
+  
+  const runGame = () => startGame(description, questionAnswer);
+  
+  export default runGame;
