c739e8187e299336eab84079ce43b046f586106e
AnnaBerk <38033737+AnnaBerk@users.noreply.github.com>
2021-02-20 08:52:10 +0000

    Update and rename prime.js to game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -2,14 +2,13 @@ import { gameData, startGame } from '../cli.js';
 import generateNumber from '../lib/generateNumber.js';
 
 const description = 'Answer "yes" if number prime otherwise answer "no".';
-
-const questionAnswer = () => {
-  const question = generateNumber(1, 100);
-  const answer = {
-  for(let i = 2; i < question; i++)
-    if(question % i === 0) return 'no';
+const question = generateNumber(1, 100);
+function isPrime(num) {
+  for(var i = 2; i < num; i++)
+    if(num % i === 0) return 'no';
   return 'yes';
 }
+const questionAnswer = isPrime(question); 
   return gameData(question, answer);
 };
 
